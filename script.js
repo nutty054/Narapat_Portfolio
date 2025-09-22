afa409211a8c5463ddb1cfe3ac7ad36a48c2a0b2
Narapat Pukkam <narapatpukkam@gmail.com>
2025-09-22 13:21:35 +0700

    Update PLS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.getElementById('menu-icon');
     const navLinks = document.querySelector('.nav-links');
@@ -18,3 +19,25 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+=======
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuIcon.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+
+    // Close the menu when a link is clicked
+    navLinks.addEventListener('click', function() {
+        navLinks.classList.remove('active');
+    });
+
+    // Close the menu if the user clicks anywhere outside of it
+    document.addEventListener('click', function(event) {
+        if (!menuIcon.contains(event.target) && !navLinks.contains(event.target)) {
+            navLinks.classList.remove('active');
+        }
+    });
+});
+>>>>>>> 87da5dd (Initial commit)
